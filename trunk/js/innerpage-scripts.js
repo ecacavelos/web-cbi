e4daaa302b43f7bca4bbbeb21e87878c21c1ee5c
david.eaj@gmail.com <david.eaj@gmail.com@1f5c0470-9d42-f910-8cf5-8f12592bf8fe>
2012-05-31 15:46:35 +0000

    Se corrigieron las transiciones entre la pagina principal y la pagina de contenidos. - Falta cambiar el estilo de los scroll bars para hacerlo mas acorde al sitio.

---
+++ trunk/js/innerpage-scripts.js
@@ -0,0 +1,84 @@
+// JavaScript Document
+$(document).ready(function() {
+	// executes when HTML-Document is loaded and DOM is ready
+	$("body").css("display", "none");	
+});
+
+$(window).load(function() {
+	// executes when complete page is fully loaded, including all frames, objects and images	
+	$("body").fadeIn(750);
+	//$("#text-area").animate({opacity: '100%'}, 500);
+	//$("#footer-line").animate({opacity: '100%'}, 500);
+	
+	var button_height_claro = $("img.claro").height()
+	var button_height_oscuro = $("img.oscuro").height();
+	$("img.claro").css("height", "19px");
+	$("img.oscuro").css("height", "19px");
+		
+	$("img.claro").hover(	
+		function () {
+			if($(this).hasClass('b1') == false) {
+				//$(this).animate({height: '70%'}, 500);
+				curHeight = $(this).height(),
+				autoHeight = $(this).css('height', 'auto').height();
+				$(this).height(curHeight).animate({height: autoHeight}, 500);
+				if($(this).hasClass('b3')) {
+					$(this).attr("src","images/boton-desarrollo.png");
+				}
+				if($(this).hasClass('b5')) {
+					$(this).attr("src","images/boton-proyectos.png");
+				}
+			}
+		}, 
+		function () {
+			if($(this).hasClass('b1') == false) {
+				$(this).animate({height: '19px'}, 500);
+				$(this).attr("src","images/boton-claro.png");
+			}
+		}
+	);
+	
+	$("img.oscuro").hover(	
+		function () {
+			//$(this).animate({height: '65%'}, 500);
+			curHeight = $(this).height(),
+			autoHeight = $(this).css('height', 'auto').height();
+			$(this).height(curHeight).animate({height: autoHeight}, 500);
+			if($(this).hasClass('b2')) {
+				$(this).attr("src","images/boton-servicios.png");
+			}
+			if($(this).hasClass('b4')) {
+				$(this).attr("src","images/boton-contacto.png");
+			}		
+		}, 
+		function () {
+			$(this).animate({height: '19px'}, 500);
+			$(this).attr("src","images/boton-oscuro.png");
+		}
+	);
+	
+	$("a.transition-out").click(function(event){
+		var clicked = $(this);
+        event.preventDefault();
+        linkLocation = this.href;
+        //$("body").fadeOut(750, redirectPage);
+		
+		$("#logo").animate({left: '45%'}, 500);
+		$("#main-section").animate({top: '40%'}, 500);
+		$("#footer-line").animate({opacity: '0%'}, 500);
+		$("#text-area").animate({opacity: '0%'}, 500, redirectPage);
+    });
+	
+	window.addEventListener("load", displayPage, false);
+	window.addEventListener("unload", displayPage, false);
+	
+	function redirectPage() {
+        window.location = linkLocation;
+		//$("#main-section").css("top", "40%");
+    }
+	
+});
+
+function displayPage() {
+
+}
