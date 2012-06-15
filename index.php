93293f9d9ea9330948fed8289c6866a02206c4d4
david.eaj@gmail.com <david.eaj@gmail.com@1f5c0470-9d42-f910-8cf5-8f12592bf8fe>
2012-06-15 22:57:21 +0000

    - Se modificó el background y pequeños cambios esteticos, como ensanchar y achicar la fuente de las paginas de contenido.

--- index.php
+++ index.php
@@ -1,47 +1,48 @@
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="robots" content="noindex" />
-        <title>CBI</title>
-        <link rel="stylesheet" type="text/css" href="global.css" />
-        <script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
-        <script type="text/javascript" src="js/frontpage-scripts.js"></script>
-		<noscript>
-        
-        </noscript>
-    </head>
-    <body>
-        <!--Script para detectar la resolucón del usuario.-->
-		<!--
-        <script type="text/javascript">
-			document.write(screen.width+'x'+screen.height);
-		</script>
-        -->
-		<div id="wrap">
-	<div id="content">
-            	<div id="logo">
-                	<img src="images/logo.png" width="160" height="80"/>
-                </div>
-				<div id="main-section"><a class="transition-up b1" href="empresa.php"><img class="claro b1" src="images/boton-empresa.png" width="196" height="143"/></a>
-                <a class="transition-up b2" href="servicios.php"><img class="oscuro b2" src="images/boton-servicios.png" width="170" height="115"/></a>
-                <a class="transition-up b3" href="desarrollo.php"><img class="claro b3" src="images/boton-desarrollo.png" width="196" height="143"/></a>
-                <a class="transition-up b4" href="clientes.php"><img class="oscuro b4" src="images/boton-contacto.png" width="170" height="115"/></a>
-                <a class="transition-up b5" href="contacto.php"><img class="claro b5" src="images/boton-proyectos.png" width="196" height="143"/></a>
-                </div>
-		    	<div id="social-icons">
-					<a target="_blank" href="http://twitter.com/#!/CBI_py"><img class="twitter" src="images/icon_tw-black.png" width="32" height="32"/></a><a target="_blank" href="http://www.facebook.com/CBInformatica"><img class="facebook" src="images/icon_fb-black.png" width="32" height="32"/></a>
-				</div>
-                <div id="footer">
-                    <div id="footer_text">
-                    	<p>C.B. Solutions S.R.L - Todos los derechos reservados
-                        <br />
-                        <a href="contacto.html" >Contacto</a>
-                        </p>    
-                    </div>
-                
-                </div>
-                
-			</div>
-    </body>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="robots" content="noindex" />
+    <title>CBI</title>
+    <link rel="stylesheet" type="text/css" href="global.css" />
+    <script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
+    <script type="text/javascript" src="js/frontpage-scripts.js"></script>
+    <noscript>
+    
+    </noscript>
+</head>
+<body>
+    <!--Script para detectar la resolucón del usuario.-->
+    <!--
+    <script type="text/javascript">
+        document.write(screen.width+'x'+screen.height);
+    </script>
+    -->
+    <div id="upper_bar"></div>
+    <div id="wrap">
+		<div id="content">
+            <div id="logo">
+                <img src="images/logo.png" width="160" height="80"/>
+            </div>
+            <div id="main-section"><a class="transition-up b1" href="empresa.php"><img class="claro b1" src="images/boton-empresa.png" width="196" height="143"/></a>
+            <a class="transition-up b2" href="servicios.php"><img class="oscuro b2" src="images/boton-servicios.png" width="170" height="115"/></a>
+            <a class="transition-up b3" href="desarrollo.php"><img class="claro b3" src="images/boton-desarrollo.png" width="196" height="143"/></a>
+            <a class="transition-up b4" href="clientes.php"><img class="oscuro b4" src="images/boton-contacto.png" width="170" height="115"/></a>
+            <a class="transition-up b5" href="contacto.php"><img class="claro b5" src="images/boton-proyectos.png" width="196" height="143"/></a>
+            </div>
+            <div id="social-icons">
+                <a target="_blank" href="http://twitter.com/#!/CBI_py"><img class="twitter" src="images/icon_tw-black.png" width="32" height="32"/></a><a target="_blank" href="http://www.facebook.com/CBInformatica"><img class="facebook" src="images/icon_fb-black.png" width="32" height="32"/></a>
+            </div>
+            <div id="footer">
+                <div id="footer_text">
+                    <p>C.B. Solutions S.R.L - Todos los derechos reservados
+                    <br />
+                    <a href="contacto.html" >Contacto</a>
+                    </p>    
+                </div>            
+            </div>
+            
+	</div>
+	<div id="lower_bar"></div>
+</body>
 </html>
