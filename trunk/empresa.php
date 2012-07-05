cb91b15816849c2bf031add5929b95d546c772af
estebancacavelos@gmail.com <estebancacavelos@gmail.com@1f5c0470-9d42-f910-8cf5-8f12592bf8fe>
2012-07-05 22:59:42 +0000

    nueva version de la pagina de empresa. Con Overview e Historia. FALTA: Equipo humano.

---
+++ trunk/empresa.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="robots" content="noindex" />
+        <title>CBI</title>
+        <link rel="stylesheet" type="text/css" href="global.css" />
+        <link rel="stylesheet" type="text/css" href="css/inner-pages.css" />
+        <script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
+        <script type="text/javascript" src="js/innerpage-scripts.js"></script>
+		<noscript>
+        
+        </noscript>
+    </head>
+    <body>
+        <!--Script para detectar la resolucón del usuario.-->
+		<!--
+        <script type="text/javascript">
+			document.write(screen.width+'x'+screen.height);
+		</script>
+        -->
+		<div id="wrap">
+			<div id="content">
+            	<div id="logo"> <a class="transition-out" href="index.html"><img src="images/logo.png" width="160" height="80"/></a>
+                </div>
+				<div id="main-section"><a class="transition b1" href="#"><img class="claro b1" src="images/boton-empresa.png" width="196" height="143"/></a><a class="transition b2" href="#"><img class="oscuro b2" src="images/boton-oscuro.png" width="170" height="115"/></a><a class="transition b3" href="#"><img class="claro b3" src="images/boton-claro.png" width="198" height="145"/></a><a class="transition b4" href="#"><img class="oscuro b4" src="images/boton-oscuro.png" width="198" height="145"/></a><a class="transition b5" href="#"><img class="claro b5" src="images/boton-claro.png" width="198" height="145"/></a></div>
+                <div id="text-area">
+                	<div id="column1">
+                    	<h2> Overview </h2>
+                    	<p>CBI es una empresa joven que busca estar a la vanguardia de las tecnologias de la información. El objetivo primordial de nuestra empresa es ofrecer un servicio de calidad posible en las areas que nos desempeñamos. Nuestra motivacion nace debido a que observamos una gran demanda de servicios de calidad en el servicio tecnico basico como asi tambien los servicios mas avanzados. </p>
+                        <h2> Historia </h2>
+                    	<p> Iniciamos nuestras actividades en julio del 2010 como proveedores de servicios tecnicos a la par que seguiamos en fases de culminación de la carrera de Ingenieria informatica en la Universidad Catolica Nuestra Señora de la Asuncion. Luego fuimos incursionando especificamente en lo relacionado a administracion de servidores Linux y varios de los servicios relacionados.   </p><br>
+                        <p>En el 2011 nos afianzamos con nuestros primeros clientes, creando un vinculo de confianza muy importante. Tambien fuimos cerrando negocios con clientes nuevos que apostaron por nosotros, hoy vemos satisfechos a varios de ellos. Por otra parte, fuimos incursionando en el desarrollo de software con pequeños trabajos. En este año se incorporaron 3 personas mas al equipo humano de CBI.</p><br>
+                        <p>En el 2012 nos afianzamos con nuestros primeros clientes, creando un vinculos de confianza muy importantes. Particularmente decidimos incursionar de lleno en lo que es desarrollo de software. Al poco tiempo hemos tenido algunos <a href="proyectos.php" target="_blank">proyectos</a> en desarrollo, con diferentes tecnologías aplicadas.</p><br>
+                    </div>                    	
+                	<div id="column2">
+                    <h2> Equipo Humano </h2>
+                    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br>
+                        <p>ffffffff .</p>
+                    </div>
+                </div>
+                <div id="footer-line">
+                </div>                
+		    	<div id="social-icons">
+					<img class="twitter" src="images/icon_tw-black.png" width="32" height="32"/><img class="facebook" src="images/icon_fb-black.png" width="32" height="32"/>
+				</div>
+			</div>
+		</div>
+    </body>
+</html>
