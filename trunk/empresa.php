c8ff4883e32047cb393fca72234064ca6313b7be
david.eaj@gmail.com <david.eaj@gmail.com@1f5c0470-9d42-f910-8cf5-8f12592bf8fe>
2012-07-06 19:30:38 +0000

    - Se corrigió el orden y etiquetas de los botones: Empresa, Servicios, Proyectos, Clientes, Contacto - Se agregaron links a los iconos sociales de la pagina de Empresa

--- trunk/empresa.php
+++ trunk/empresa.php
@@ -1,61 +1,65 @@
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="robots" content="noindex" />
-        <title>CBI</title>
-        <link rel="stylesheet" type="text/css" href="global.css" />
-        <link rel="stylesheet" type="text/css" href="css/inner-pages.css" />
-        <script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
-        <script type="text/javascript" src="js/innerpage-scripts.js"></script>
-		<noscript>
-        
-        </noscript>
-    </head>
-    <body>
-        <!--Script para detectar la resolucón del usuario.-->
-		<!--
-        <script type="text/javascript">
-			document.write(screen.width+'x'+screen.height);
-		</script>
-        -->
-        <div id="upper_bar"></div>
-		<div id="wrap">
-			<div id="content">
-            	<div id="logo"> <a class="transition-out" href="index.php"><img src="images/logo.png" width="160" height="80"/></a>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="robots" content="noindex" />
+    <title>CBI</title>
+    <link rel="stylesheet" type="text/css" href="global.css" />
+    <link rel="stylesheet" type="text/css" href="css/inner-pages.css" />
+    <script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
+    <script type="text/javascript" src="js/innerpage-scripts.js"></script>
+    <noscript>
+    
+    </noscript>
+</head>
+<body>
+    <!--Script para detectar la resolucón del usuario.-->
+    <!--
+    <script type="text/javascript">
+        document.write(screen.width+'x'+screen.height);
+    </script>
+    -->
+    <div id="upper_bar"></div>
+    <div id="wrap">
+        <div id="content">
+            <div id="logo"> <a class="transition-out" href="index.php"><img src="images/logo.png" width="160" height="80"/></a></div>
+            <div id="main-section"><a class="transition b1" href="#"><img class="claro b1" src="images/boton-empresa.png" width="196" height="143"/></a>
+                <a class="transition b2" href="servicios.php"><img class="oscuro b2" src="images/boton-oscuro.png" width="170" height="115"/></a>
+                <a class="transition b3" href="proyectos.php"><img class="claro b3" src="images/boton-claro.png" width="198" height="145"/></a>
+                <a class="transition b4" href="clientes.php"><img class="oscuro b4" src="images/boton-oscuro.png" width="198" height="145"/></a>
+                <a class="transition b5" href="contacto.php"><img class="claro b5" src="images/boton-claro.png" width="198" height="145"/></a>
+            </div>
+            <div id="text-area">
+                <div id="column1">
+                    <h2> Overview </h2>
+                    <p>CBI es una empresa joven que busca estar a la vanguardia de las tecnologias de la información. El objetivo primordial de nuestra empresa es ofrecer un servicio de calidad posible en las areas que nos desempeñamos. Nuestra motivacion nace debido a que observamos una gran demanda de servicios de calidad en el servicio tecnico basico como asi tambien los servicios mas avanzados.</p>
+                    <h2> Historia </h2>
+                    <p>Iniciamos nuestras actividades en julio del 2010 como proveedores de servicios tecnicos a la par que seguiamos en fases de culminación de la carrera de Ingenieria informatica en la Universidad Catolica Nuestra Señora de la Asuncion. Luego fuimos incursionando especificamente en lo relacionado a administracion de servidores Linux y varios de los servicios relacionados.</p>
+                    <p>En el 2011 nos afianzamos con nuestros primeros clientes, creando un vinculo de confianza muy importante. Tambien fuimos cerrando negocios con clientes nuevos que apostaron por nosotros, hoy vemos satisfechos a varios de ellos. Por otra parte, fuimos incursionando en el desarrollo de software con pequeños trabajos. En este año se incorporaron 3 personas mas al equipo humano de CBI.</p>
+                    <p>En el 2012 nos afianzamos con nuestros primeros clientes, creando un vinculos de confianza muy importantes. Particularmente decidimos incursionar de lleno en lo que es desarrollo de software. Al poco tiempo hemos tenido algunos <a href="proyectos.php" target="_blank">proyectos</a> en desarrollo, con diferentes tecnologías aplicadas.</p>
+                </div>                    	
+                <div id="column2">
+                <h2> Equipo Humano </h2>
+                    <p>El material mas importante que tiene CBI, es sin duda, el humano. CBI esta compuesto por un equipo de jovenes profesionales y en etapa de desarrollo en carreras relacionadas a la Informática. </p>
+                    <p>Cuidamos este patrimonio estimulando la capacitación constante de todas las personas que trabajan en nuestra empresa. Tenemos como política, el apoyo en la realización de cursos o actividades por parte del staff de CBI y de esta manera, contribuir con la sociedad, con personas mejor preparadas.</p>
+                    <p>Tambien prestamos mucha atención al desarrollo integral de la persona, fomentando siempre valores como honestidad, respeto, trabajo en equipo y autosuficiencia. Para nosotros es importante entender y promover la idea que el lugar de trabajo debe ser, un lugar de educación constante.  </p>
                 </div>
-				<div id="main-section"><a class="transition b1" href="#"><img class="claro b1" src="images/boton-empresa.png" width="196" height="143"/></a><a class="transition b2" href="servicios.php"><img class="oscuro b2" src="images/boton-oscuro.png" width="170" height="115"/></a><a class="transition b3" href="proyectos.php"><img class="claro b3" src="images/boton-claro.png" width="198" height="145"/></a><a class="transition b4" href="clientes.php"><img class="oscuro b4" src="images/boton-oscuro.png" width="198" height="145"/></a><a class="transition b5" href="contacto.php"><img class="claro b5" src="images/boton-claro.png" width="198" height="145"/></a></div>
-                <div id="text-area">
-                	<div id="column1">
-                    	<h2> Overview </h2>
-                    	<p>CBI es una empresa joven que busca estar a la vanguardia de las tecnologias de la información. El objetivo primordial de nuestra empresa es ofrecer un servicio de calidad posible en las areas que nos desempeñamos. Nuestra motivacion nace debido a que observamos una gran demanda de servicios de calidad en el servicio tecnico basico como asi tambien los servicios mas avanzados.</p>
-                        <h2> Historia </h2>
-                    	<p>Iniciamos nuestras actividades en julio del 2010 como proveedores de servicios tecnicos a la par que seguiamos en fases de culminación de la carrera de Ingenieria informatica en la Universidad Catolica Nuestra Señora de la Asuncion. Luego fuimos incursionando especificamente en lo relacionado a administracion de servidores Linux y varios de los servicios relacionados.</p>
-                        <p>En el 2011 nos afianzamos con nuestros primeros clientes, creando un vinculo de confianza muy importante. Tambien fuimos cerrando negocios con clientes nuevos que apostaron por nosotros, hoy vemos satisfechos a varios de ellos. Por otra parte, fuimos incursionando en el desarrollo de software con pequeños trabajos. En este año se incorporaron 3 personas mas al equipo humano de CBI.</p>
-                        <p>En el 2012 nos afianzamos con nuestros primeros clientes, creando un vinculos de confianza muy importantes. Particularmente decidimos incursionar de lleno en lo que es desarrollo de software. Al poco tiempo hemos tenido algunos <a href="proyectos.php" target="_blank">proyectos</a> en desarrollo, con diferentes tecnologías aplicadas.</p>
-                    </div>                    	
-                	<div id="column2">
-                    <h2> Equipo Humano </h2>
-                    	<p>El material mas importante que tiene CBI, es sin duda, el humano. CBI esta compuesto por un equipo de jovenes profesionales y en etapa de desarrollo en carreras relacionadas a la Informática. </p>
-                    	<p>Cuidamos este patrimonio estimulando la capacitación constante de todas las personas que trabajan en nuestra empresa. Tenemos como política, el apoyo en la realización de cursos o actividades por parte del staff de CBI y de esta manera, contribuir con la sociedad, con personas mejor preparadas.</p>
-                        <p>Tambien prestamos mucha atención al desarrollo integral de la persona, fomentando siempre valores como honestidad, respeto, trabajo en equipo y autosuficiencia. Para nosotros es importante entender y promover la idea que el lugar de trabajo debe ser, un lugar de educación constante.  </p>
-                    </div>
-                </div>
-                <div id="footer-line">
-                </div>                
-		    	<div id="social-icons">
-					<img class="twitter" src="images/icon_tw-black.png" width="32" height="32"/><img class="facebook" src="images/icon_fb-black.png" width="32" height="32"/>
-				</div>
-                <div id="footer">
-                	<div id="footer_text">
-                        <p>C.B. Solutions S.R.L - Todos los derechos reservados
-                        <br>
-                        <a href="contacto.php" >Contacto</a>
-                        </p>    
-	                </div>            
-    	        </div>
-			</div>
-		</div>
+            </div>
+            <div id="footer-line">
+            </div>                
+            <div id="social-icons">
+                <a target="_blank" href="http://twitter.com/#!/CBI_py"><img class="twitter" src="images/icon_tw-black.png" width="32" height="32"/></a><a target="_blank" href="http://www.facebook.com/CBInformatica"><img class="facebook" src="images/icon_fb-black.png" width="32" height="32"/></a>
+            </div>
+            <div id="footer">
+                <div id="footer_text">
+                    <p>C.B. Solutions S.R.L - Todos los derechos reservados
+                    <br>
+                    <a href="contacto.php" >Contacto</a>
+                    </p>    
+                </div>            
+            </div>
+        </div>
+    </div>
 	<div id="lower_bar"></div>
 </body>
 </html>
--- trunk/index.php
+++ trunk/index.php
@@ -21,14 +21,12 @@
     <div id="upper_bar"></div>
     <div id="wrap">
 		<div id="content">
-            <div id="logo">
-                <img src="images/logo.png" width="160" height="80"/>
-            </div>
-                <div id="main-section"><a class="transition-up b1" href="empresa.php"><img class="claro b1" src="images/boton-empresa.png" width="196" height="143"/></a>
-                <a class="transition-up b5" href="servicios.php"><img class="claro b5" src="images/boton-servicios.png" width="196" height="143"/></a>
-                <a class="transition-up b5" href="proyectos.php"><img class="claro b5" src="images/boton-proyectos.png" width="196" height="143"/></a>
-                <a class="transition-up b5" href="clientes.php"><img class="claro b5" src="images/boton-clientes.png" width="196" height="143"/></a>
-                <a class="transition-up b4" href="contacto.php"><img class="oscuro b4" src="images/boton-contacto.png" width="170" height="115"/></a>
+            <div id="logo"><img src="images/logo.png" width="160" height="80"/></div>
+			<div id="main-section"><a class="transition-up b1" href="empresa.php"><img class="claro b1" src="images/boton-empresa.png" width="196" height="143"/></a>
+                <a class="transition-up b2" href="servicios.php"><img class="claro b2" src="images/boton-servicios.png" width="196" height="143"/></a>
+                <a class="transition-up b3" href="proyectos.php"><img class="claro b3" src="images/boton-proyectos.png" width="196" height="143"/></a>
+                <a class="transition-up b4" href="clientes.php"><img class="claro b4" src="images/boton-clientes.png" width="196" height="143"/></a>
+                <a class="transition-up b5" href="contacto.php"><img class="oscuro b5" src="images/boton-contacto.png" width="170" height="115"/></a>
             </div>
             <div id="social-icons">
                 <a target="_blank" href="http://twitter.com/#!/CBI_py"><img class="twitter" src="images/icon_tw-black.png" width="32" height="32"/></a><a target="_blank" href="http://www.facebook.com/CBInformatica"><img class="facebook" src="images/icon_fb-black.png" width="32" height="32"/></a>
