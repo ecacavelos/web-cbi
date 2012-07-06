cdd9a68bb4d569c61b3e5596b81a7dd181e22248
estebancacavelos@gmail.com <estebancacavelos@gmail.com@1f5c0470-9d42-f910-8cf5-8f12592bf8fe>
2012-07-06 20:27:53 +0000

    Pagina de servicios con el menu correcto. inner-paes.css se modifico para que tome el valor de la variable en cada archivo

---
+++ servicios.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="robots" content="noindex" />
+        <title>CBI</title>
+        <link rel="stylesheet" type="text/css" href="global.css" />
+        <link rel="stylesheet" type="text/css" href="css/inner-pages.css" />
+		<style type="text/css" id="page-css">
+			#main-section img.b2 {
+				height: auto !important;
+			}
+		</style>        
+        <script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
+        <script type="text/javascript" src="js/innerpage-scripts.js"></script>
+       <script type="text/javascript" id="sourcecode">
+        $(function()
+        {
+            $currentpageclass = 'b2';
+        });
+    </script>
+		<noscript>
+        
+        </noscript>
+    </head>
+    <body>
+        <!--Script para detectar la resolucón del usuario.-->
+		<!--
+        <script type="text/javascript">
+			document.write(screen.width+'x'+screen.height);
+		</script>
+        -->
+		<div id="wrap">
+			<div id="content">
+            	<div id="logo"> <a class="transition-out" href="index.php"><img src="images/logo.png" width="160" height="80"/></a>
+                </div>
+				<div id="main-section"><a class="transition b1" href="#"><img class="claro b1" src="images/boton-claro.png" width="196" height="143"/></a><a class="transition b2_1" href="servicios.php"><img class="oscuro b2" src="images/boton-servicios.png" width="170" height="115"/></a><a class="transition b3" href="proyectos.php"><img class="claro b3" src="images/boton-claro.png" width="198" height="145"/></a><a class="transition b4" href="clientes.php"><img class="oscuro b4" src="images/boton-oscuro.png" width="198" height="145"/></a><a class="transition b5" href="contacto.php"><img class="claro b5" src="images/boton-claro.png" width="198" height="145"/></a></div>
+                <div id="text-area">
+                	
+                    
+                </div>
+                <div id="footer-line">
+                </div>                
+		    	<div id="social-icons">
+					<img class="twitter" src="images/icon_tw-black.png" width="32" height="32"/><img class="facebook" src="images/icon_fb-black.png" width="32" height="32"/>
+				</div>
+                <div id="footer">
+                	<div id="footer_text">
+                        <p>C.B. Solutions S.R.L - Todos los derechos reservados
+                        <br />
+                        <a href="contacto.php" >Contacto</a>
+                        </p>    
+	                </div>            
+    	        </div>
+			</div>
+		</div>
+    </body>
+</html>
