4354800f4541787a01fe6cffa5a2ae57a8918753
estebancacavelos@gmail.com <estebancacavelos@gmail.com@1f5c0470-9d42-f910-8cf5-8f12592bf8fe>
2012-07-09 22:43:50 +0000

    Se agrego la primera version de la pagina donde van a estar los servicios. Se agregaron las reglas css para utilzadas en esta pagina.

--- trunk/servicios.php
+++ trunk/servicios.php
@@ -1,58 +1,77 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="robots" content="noindex" />
-    <title>CBI</title>
-    <link rel="stylesheet" type="text/css" href="global.css" />
-    <link rel="stylesheet" type="text/css" href="css/inner-pages.css" />
-    <style type="text/css" id="page-css">
-        #main-section img.b2 {
-            height: auto !important;
-        }
-    </style>        
-    <script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
-    <script type="text/javascript" src="js/innerpage-scripts.js"></script>
-    <script type="text/javascript" id="sourcecode">
-        $(function()
-        {
-            $currentpageclass = 'b2';
-        });
-    </script>
-    <noscript>
-    
-    </noscript>
-</head>
-    <body>
-    <div id="upper_bar"></div>
-    <div id="wrap">
-        <div id="content">
-            <div id="logo"><a class="transition-out" href="index.php"><img src="images/logo.png" width="160" height="80"/></a>
-            </div>
-            <div id="main-section"><a class="transition b1" href="empresa.php"><img class="claro b1" src="images/boton-claro.png" width="196" height="143"/></a>
-            	<a class="transition b2_1" href="servicios.php"><img class="oscuro b2" src="images/boton-servicios.png" width="170" height="115"/></a>
-                <a class="transition b3" href="proyectos.php"><img class="claro b3" src="images/boton-claro.png" width="198" height="145"/></a>
-                <a class="transition b4" href="clientes.php"><img class="oscuro b4" src="images/boton-oscuro.png" width="198" height="145"/></a>
-                <a class="transition b5" href="contacto.php"><img class="claro b5" src="images/boton-claro.png" width="198" height="145"/></a></div>
-            <div id="text-area">
-                
-                
-            </div>
-            <div id="footer-line">
-            </div>                
-            <div id="social-icons">
-                <img class="twitter" src="images/icon_tw-black.png" width="32" height="32"/><img class="facebook" src="images/icon_fb-black.png" width="32" height="32"/>
-            </div>
-            <div id="footer">
-                <div id="footer_text">
-                    <p>C.B. Solutions S.R.L - Todos los derechos reservados
-                    <br />
-                    <a href="contacto.php" >Contacto</a>
-                    </p>    
-                </div>            
-            </div>
-        </div>
-    </div>
-    <div id="lower_bar"></div>
-</body>
-</html>
+  <!DOCTYPE html>
+  <html lang="es">
+  <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="robots" content="noindex" />
+      <title>CBI</title>
+      <link rel="stylesheet" type="text/css" href="global.css" />
+      <link rel="stylesheet" type="text/css" href="css/inner-pages.css" />
+      <style type="text/css" id="page-css">
+          #main-section img.b2 {
+              height: auto !important;
+          }
+      </style>        
+      <script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
+      <script type="text/javascript" src="js/innerpage-scripts.js"></script>
+      <script type="text/javascript" id="sourcecode">
+          $(function()
+          {
+              $currentpageclass = 'b2';
+          });
+      </script>
+      <noscript>
+      
+      </noscript>
+  </head>
+      <body>
+      <div id="upper_bar"></div>
+      <div id="wrap">
+          <div id="content">
+              <div id="logo"><a class="transition-out" href="index.php"><img src="images/logo.png" width="160" height="80"/></a>
+              </div>
+              <div id="main-section"><a class="transition b1" href="empresa.php"><img class="claro b1" src="images/boton-claro.png" width="196" height="143"/></a>
+                  <a class="transition b2_1" href="servicios.php"><img class="oscuro b2" src="images/boton-servicios.png" width="170" height="115"/></a>
+                  <a class="transition b3" href="proyectos.php"><img class="claro b3" src="images/boton-claro.png" width="198" height="145"/></a>
+                  <a class="transition b4" href="clientes.php"><img class="oscuro b4" src="images/boton-oscuro.png" width="198" height="145"/></a>
+                  <a class="transition b5" href="contacto.php"><img class="claro b5" src="images/boton-claro.png" width="198" height="145"/></a></div>
+              <div id="text-area">
+                  <div class="services_row">
+                      <h2>Help Desk</h2>
+                       <img src="images/technology_consulting_picture.jpg" alt="IT CONSULT" width="364" height="223" class="services_row_image">
+                      <p>Este servicio consiste en ofrecer a empresas y personas individuales una asistencia in-situ y de forma remota, con el fin de resolver problemas de usuario final. Los problemas de usuario final, son los que normalmente se le presentan al usuario que no tiene muchos conocimientos. </p>
+      
+      <p>Con este servicio pretendemos ofrecer un método preventivo, mediante planes periódicos de visitas de mantenimiento en el lugar del cliente, realizando tareas básicas. Algunas de las tareas desarrolladas son:</p>
+      
+      <p>➢	Chequeos de seguridad en las terminales: Antivirus, firewall.</p>
+      <p>➢	Limpieza física de equipos.</p>
+      <p>➢	Configuraciones de programas de usuario: Clientes de correo, Procesadores de texto, etc.</p>
+      <p>➢	Instalación y configuraciones de impresoras.</p>
+      </p>     
+                  </div>
+                  
+             <div class="services_row">
+                   <h2>Consultoría IT</h2>	
+               <img src="images/technology_consulting_picture.jpg" alt="IT CONSULT" width="364" height="223" class="services_row_image"> <p>Nuestro servicio de consultoría consiste en brindar al cliente un análisis y una posterior recomendación cuando este precise realizar algún cambio estructural en su infraestructura informática (software y hardware).</p>
+  <p>También ofrecemos este servicio al momento de comprar equipamiento informático, ya que contamos con proveedores mayoristas. De este modo, realizamos una venta de en puerta, sin que el cliente tenga que preocuparse por movilizarse para comprar y mucho menos instalar y configurar los equipos.</p> 
+  <p>Por ultimo, ofrecemos consultoría cuando se quiere una solución nueva o cambiar una existente respecto a un sistema. Contamos con varios productos desarrollados y relaciones de trabajo con importantes empresas de desarrollo de software.</p>
+                  </div>
+                   
+              </div>
+              <div id="footer-line">
+              </div>                
+              <div id="social-icons">
+                  <img class="twitter" src="images/icon_tw-black.png" width="32" height="32"/><img class="facebook" src="images/icon_fb-black.png" width="32" height="32"/>
+              </div>
+              <div id="footer">
+                  <div id="footer_text">
+                      <p>C.B. Solutions S.R.L - Todos los derechos reservados
+                      <br />
+                      <a href="contacto.php" >Contacto</a>
+                      </p>    
+                  </div>            
+              </div>
+          </div>
+      </div>
+      <div id="lower_bar"></div>
+  </body>
+  </html>
