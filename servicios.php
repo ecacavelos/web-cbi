52a8ab6de4c100e40439e482b348eb61d961274d
estebancacavelos@gmail.com <estebancacavelos@gmail.com@1f5c0470-9d42-f910-8cf5-8f12592bf8fe>
2012-07-10 22:51:24 +0000

    pagina final de srevicios

--- servicios.php
+++ servicios.php
@@ -3,9 +3,10 @@
   <head>
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
       <meta name="robots" content="noindex" />
-      <title>CBI</title>
+      <title>    :::CBI Consultora Informática:::</title>
       <link rel="stylesheet" type="text/css" href="global.css" />
       <link rel="stylesheet" type="text/css" href="css/inner-pages.css" />
+      <link rel="shortcut icon" href="images/logo--favicon.ico">
       <style type="text/css" id="page-css">
           #main-section img.b2 {
               height: auto !important;
@@ -37,8 +38,8 @@
               <div id="text-area">
                   <div class="services_row">
                       <h2>Help Desk</h2>
-                       <img src="images/technology_consulting_picture.jpg" alt="IT CONSULT" width="364" height="223" class="services_row_image">
-                      <p>Este servicio consiste en ofrecer a empresas y personas individuales una asistencia in-situ y de forma remota, con el fin de resolver problemas de usuario final. Los problemas de usuario final, son los que normalmente se le presentan al usuario que no tiene muchos conocimientos. </p>
+                       <img src="images/help_desk.jpg" alt="IT CONSULT" width="364" height="223" class="services_row_image">
+                      <div class="services_row_text"><p>Este servicio consiste en ofrecer a empresas y personas individuales una asistencia in-situ y de forma remota, con el fin de resolver problemas de usuario final. Los problemas de usuario final, son los que normalmente se le presentan al usuario que no tiene muchos conocimientos. </p>
       
       <p>Con este servicio pretendemos ofrecer un método preventivo, mediante planes periódicos de visitas de mantenimiento en el lugar del cliente, realizando tareas básicas. Algunas de las tareas desarrolladas son:</p>
       
@@ -47,13 +48,38 @@
       <p>➢	Configuraciones de programas de usuario: Clientes de correo, Procesadores de texto, etc.</p>
       <p>➢	Instalación y configuraciones de impresoras.</p>
       </p>     
+      </div>
                   </div>
                   
              <div class="services_row">
                    <h2>Consultoría IT</h2>	
-               <img src="images/technology_consulting_picture.jpg" alt="IT CONSULT" width="364" height="223" class="services_row_image"> <p>Nuestro servicio de consultoría consiste en brindar al cliente un análisis y una posterior recomendación cuando este precise realizar algún cambio estructural en su infraestructura informática (software y hardware).</p>
+               <img src="images/technology_consulting_picture.jpg" alt="IT CONSULT" width="364" height="223" class="services_row_image">
+               <div class="services_row_text"> <p>Nuestro servicio de consultoría consiste en brindar al cliente un análisis y una posterior recomendación cuando este precise realizar algún cambio estructural en su infraestructura informática (software y hardware).</p>
   <p>También ofrecemos este servicio al momento de comprar equipamiento informático, ya que contamos con proveedores mayoristas. De este modo, realizamos una venta de en puerta, sin que el cliente tenga que preocuparse por movilizarse para comprar y mucho menos instalar y configurar los equipos.</p> 
   <p>Por ultimo, ofrecemos consultoría cuando se quiere una solución nueva o cambiar una existente respecto a un sistema. Contamos con varios productos desarrollados y relaciones de trabajo con importantes empresas de desarrollo de software.</p>
+                 </div>
+                  </div>
+                  <div class="services_row">
+                   <h2>Networking</h2>	
+               <img src="images/networking.jpg" alt="IT CONSULT" width="364" height="223" class="services_row_image">
+               <div class="services_row_text"> <p>El manejo de la infraestructura de red de una empresa es un punto fundamental para mantener agiles toda la logística. Este servicio consiste en proveer las soluciones para los problemas de red que se puedan presentar. Algunos de los servicios son:</p>
+<p>•	Configuración de VPN.</p>
+<p>•	Montaje y configuración de Routers y Switchs.</p>
+<p>•	Diseño de infraestructura de red.</p>
+<p>•	Mantenimiento de infraestructura.</p>
+<p>•	Filtrado de contenido de la red.</p>
+</div>
+                  </div>
+ <div class="services_row">
+                   <h2>Servidores Linux</h2>	
+               <img src="images/linux-distros.jpg" alt="IT CONSULT" width="364" height="223" class="services_row_image">
+               <div class="services_row_text"> <p>En muchas empresas se instalan servidores Linux para ofrecer multiples servicios, muchos de ellos fuertemente relacionados con el Networking. En los servidores Linux se pueden implementar muchos servicios, como por ejemplo:</p>
+<p>•	Servidor de archivos (SAMBA).</p>
+<p>•	Servidor proxy (control de trafico de internet).</p>
+<p>•	Firewall (Control de trafico de la red).</p>
+<p>•	OpenVPN.</p>
+<p> Contamos con un conocimiento de los principales conceptos del sistema operativo, lo cual nos permite implementar rápidamente nuevos sevicios.</p>
+</div>
                   </div>
                    
               </div>
