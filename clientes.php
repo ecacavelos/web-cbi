be2446ca6b520278da5da10a1ebb0faf16936322
david.eaj@gmail.com <david.eaj@gmail.com@1f5c0470-9d42-f910-8cf5-8f12592bf8fe>
2012-07-12 14:16:38 +0000

    - Se terminó la pagina de clientes.php - Se optimizó el funcionamiento de la pagina servicios.php a varias resoluciones.

--- clientes.php
+++ clientes.php
@@ -40,7 +40,7 @@
                 	<div class="client_list_item"><img src="images/clientes/wind-expert-logo-100.png"><div class="client_list_caption">Madrigal</div></div>
                 </div>
                 <div id="column2">
-	                <div class="client_list_item"><img src="images/clientes/RTEmagicC_G_Data_25Y-Logo_550x550_13.png"><div class="client_list_caption">Mutual</div></div>
+	                <div class="client_list_item"><img src="images/clientes/RTEmagicC_G_Data_25Y-Logo_550x550_13.png"><div class="client_list_caption"><h3>Lorem ipsum</h3>Dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div></div>
     	            <div class="client_list_item"><img src="images/clientes/elcome.png"><div class="client_list_caption">Agrofuturo</div></div>
 				</div>
             </div>              
